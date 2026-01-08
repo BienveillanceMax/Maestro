0b0766858bef37a3def691ce6c72fcd1ce0f970e
Pierre Wagniart <pierre.wagniart@epita.fr>
2026-01-08 16:28:15 +0100

    first commit

---
+++ Maetro-MVP/src/main/java/com/arcos/maetromvp/MaetroMvpApplication.java
@@ -0,0 +1,14 @@
+package com.arcos.maetromvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaetroMvpApplication
+{
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaetroMvpApplication.class, args);
+    }
+
+}
---
+++ Maetro-MVP/src/test/java/com/arcos/maetromvp/MaetroMvpApplicationTests.java
@@ -0,0 +1,14 @@
+package com.arcos.maetromvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaetroMvpApplicationTests
+{
+
+    @Test
+    void contextLoads() {
+    }
+
+}
