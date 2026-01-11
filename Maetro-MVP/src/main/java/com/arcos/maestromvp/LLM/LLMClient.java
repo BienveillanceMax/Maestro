ac87e502c21d297cea494bfc800e3735c6fdd82f
Pierre Wagniart <pierre.wagniart@epita.fr>
2026-01-11 02:44:05 +0100

    feat : llm client

--- Maetro-MVP/src/main/java/com/arcos/maestromvp/LLM/LLMClient.java
+++ Maetro-MVP/src/main/java/com/arcos/maestromvp/LLM/LLMClient.java
@@ -1,10 +1,24 @@
 package com.arcos.maestromvp.LLM;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.stereotype.Component;
 
 @Component
 public class LLMClient
 {
-    public void call() {
+    ChatClient chatClient;
+
+    public LLMClient(ChatClient.Builder chatClientBuilder){
+        this.chatClient = chatClientBuilder.build();
     }
+
+    public String generateChatResponse(Prompt prompt) {
+        return chatClient.prompt(prompt)
+                .tools()
+                .call()
+                .content();
+    }
+
+
 }
